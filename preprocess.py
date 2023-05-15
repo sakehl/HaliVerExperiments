646105c0fefe006088073638c7f12288e47999cf
Lars <l.b.vandenhaak@gmail.com>
2023-05-15 14:57:42 +0200

    Update to last experiments

--- preprocess.py
+++ preprocess.py
@@ -41,28 +41,14 @@ def is_other_func(line: str) -> bool:
 # Do not count empty lines, or lines that start with "{" or "}"
 def is_none_line(line: str) -> bool:
     l = line.strip()
-    return l == "" or l.startswith("{") or l.startswith("}")
+    return l == "" or l.startswith("{") or l.startswith("}") or l.startswith("//")
 
-def count_lines(name: str, version: str, is_front: bool = False) -> LineInfo:
+def count_lines(name: str) -> LineInfo:
     lines_of_code: int = 0
     nr_annotations: int = 0
     loops: int = 0
-    if is_front:
-        pvl_name = 'build/' + name + '_front_pvl.pvl'
-    else:
-        pvl_name = 'build/' + name + '_pvl-' + version + '.pvl'
-    with open(pvl_name) as f:
+    with open(name) as f:
         for line in f:
-            # skip empty lines or lines with only braces
-            if is_none_line(line):
-               continue
-            # Skip helper functions 
-            if not is_front and is_other_func(line):
-                lines_of_code = 0
-                nr_annotations = 0
-                loops = 0
-                continue
-            
             if is_annotation(line):
                 nr_annotations += 1
             else:
@@ -76,24 +62,43 @@ def get_average(xs: List[Tuple[float, Result]]) -> Tuple[Optional[float], int, O
     total = 0.0
     passes = 0
     fails = 0
+    passtime = 0.0
     failtime = 0.0
     timeouts = 0
     for (t,r) in xs:
         if(r == "pass"):
             total += t
+            passtime += t
             passes += 1
         elif(r == "fail"):
+            total += t
             failtime += t
             fails += 1
         elif(r == "timeout"):
             timeouts += 1
-    avr_total = total / passes if passes != 0 else None
+    avr_total = total / (passes + fails) if (passes + fails) != 0 else None
+    passtime = passtime / passes if passes != 0 else None
     avr_fails = failtime / fails if fails != 0 else None
-    return avr_total, passes, failtime, fails, timeouts
+    return avr_total, passes, fails,  timeouts, passtime, failtime
+
+def average_to_str(xs: List[Tuple[float, Result]], name: str)-> str:
+    t, passes, fails,  timeouts, passtime, failtime = get_average(xs)
+    res: str
+    if((passes > 0 and passes < len(xs))):
+        print(f"inconsistent results for '{name}'")
+        print(f"avr_total, passes, fails,  timeouts, passtime, failtime: {t, passes, fails,  timeouts, passtime, failtime}")
+        res = str(round(t)) + "$^{\dag}$"
+    elif(passes == len(xs) and t != None):
+        res = f"{round(t)}" # type: ignore
+    elif(fails > 0):
+        res = "F"
+    elif(timeouts > 0):
+        res = "T.O."
+    return res
 
 
 class Encoder(json.JSONEncoder):
-    def default(self, o):
+    def default(self, o): # type: ignore
         if(isinstance(o, LineInfo)):
             return o.__dict__
         return json.JSONEncoder.default(self, o)
@@ -115,18 +120,15 @@ def as_line_info(dct: Dict[str, Any]) -> Union[Dict[str,Any], LineInfo]:
     else:
         return dct
 
-def read_results(prefix: str, name: str) -> Tuple[VerificationResults, Dict[str,float], Dict[str, LineInfo]]:
-    pre = f'{prefix}_{name}'
+def read_results(prefix: str) -> Tuple[VerificationResults, Dict[str, LineInfo]]:
+    pre = f'{prefix}'
     with open(pre + "_results_verification.json") as f:
         results_verification = json.loads(f.read(), object_hook=as_ver_result)
-        
-    with open(pre + '_compilation.json') as f:
-        compilation = json.loads(f.read(), object_hook=as_line_info)
         
     with open(pre + '_line_info.json') as f:
         line_info = json.loads(f.read(), object_hook=as_line_info)
         
-    return results_verification, compilation, line_info
+    return results_verification, line_info
 
 def runCommand(command: str, verbose: bool = False, timeout: Optional[int] = None)-> Tuple[float, Result]:
     start = time.time()
@@ -150,100 +152,83 @@ def runCommand(command: str, verbose: bool = False, timeout: Optional[int] = Non
     return end - start, Result.Pass
 
 class Experiments:
-    def __init__(self, versions: Dict[str, List[str]], repetitions: int = 5, timeout: int = 10*60):
+    def __init__(self, versions: Dict[str, List[str]], mem_versions: Dict[str, List[str]], repetitions: int = 5, timeout: int = 10*60):
         self.versions: Dict[str, List[str]] = versions
-        self.compilation_times: Dict[str, Dict[str,float]] = {}
-        self.line_infos: Dict[str, Dict[str,LineInfo]] = {}
-        self.verification_times: Dict[str, Dict[str, List[Tuple[float, Result]]]] = {}
+        self.mem_versions: Dict[str, List[str]] = mem_versions
+        self.line_infos: Dict[str,LineInfo] = {}
+        self.verification_times: Dict[str, List[Tuple[float, Result]]] = {}
 
         self.repetitions = repetitions
         self.timeout = timeout
 
-        for name in self.versions:
-            self.verification_times[name] = {}
-            self.compilation_times[name] = {}
-            self.line_infos[name] = {}
-
-    def __str__(self) -> None:
+    def __str__(self) -> str:
         result = "{"
         result += "verification_times: " + str(self.verification_times)
-        result += "\n, compilation_times: " + str(self.compilation_times)
-        result += "\n, line_infos: " + str(self.compilation_times)
         result += "\n}"
         return result
 
+    def version_to_file_name(self, name: str, version: str, mem: bool = False) -> str:
+        return name + ('_' + version if version != '' else '') + ('_mem' if mem else '') + '.pvl'
 
-
-    def make_files(self, name: str) -> None:
-        try:
-            pvl_name = 'build/' + name + '_pvl.pvl'
-            compileTimeStage1, _ = runCommand('cmake --build build -t ' + name + ' --clean-first')
-        
-            for v in self.versions[name]:
-                compileTimeStage2 = runCommand('cd build && ./' + name + ' ' + v)[0]
-                self.compilation_times[name][v] = compileTimeStage1 + compileTimeStage2
-                print("Compilation time ", v, compileTimeStage1 + compileTimeStage2)
-                line_info = count_lines(name, v)
-                self.line_infos[name][v] = line_info
+    def count_files(self) -> None:
+        for n in self.versions:
+            for v in self.versions[n]:
+                name = self.version_to_file_name(n, v)
+                line_info = count_lines("build/" + name)
+                self.line_infos[name] = line_info
+                print(name)
                 print(line_info)
-                
-            compileTimeStage2 = runCommand('cd build && ./' + name + ' ' + 'front')[0]
-            self.compilation_times[name]['front'] = compileTimeStage1 + compileTimeStage2
-            print("Compilation time front", compileTimeStage1 + compileTimeStage2)
-        except Exception as e:
-            print(e)
-
-    def front_end(self, name: str, repetitions: Optional[int] = None, timeout: Optional[int] = None) -> None:
-        n = self.repetitions if repetitions == None else repetitions
-        t = self.timeout if timeout == None else timeout
+            # Count front file
+            v = 'front'
+            name = self.version_to_file_name(n, v)
+            line_info = count_lines("build/" + name)
+            self.line_infos[name] = line_info
+            print(name)
+            print(line_info)
+        for n in self.mem_versions:
+            for v in self.mem_versions[n]:
+                name = self.version_to_file_name(n, v, True)
+                line_info = count_lines("build/" + name)
+                self.line_infos[name] = line_info
+                print(name)
+                print(line_info)
+    
+    def run_verification(self, file_name: str, repetitions: Optional[int] = None, timeout: Optional[int] = None)-> None:
+        n: int = repetitions if repetitions != None else  self.repetitions # type: ignore
+        t = timeout if timeout != None else self.timeout
         try:
-            # Front-end verification
-            pvl_name = name + '_front_pvl.pvl'
-            self.verification_times[name]['front'] = []
+            self.verification_times[file_name] = []
             for i in range(0,n):
-                verificationTime = runCommand('vct --backend-option --conditionalizePermissions --silicon-quiet build/' + pvl_name, timeout=t)
-                self.verification_times[name]['front'].append(verificationTime)
+                verificationTime = runCommand('vct --backend-option --conditionalizePermissions --silicon-quiet build/' + file_name, timeout=t)
+                self.verification_times[file_name].append(verificationTime)
                 print(verificationTime)
         except Exception as e:
             print(e)
 
-    def back_end(self, name: str, repetitions: Optional[int] = None, timeout: Optional[int] = None, version: str ='all') -> None:
-        n = self.repetitions if repetitions == None else repetitions
-        t = self.timeout if timeout == None else timeout
-        try:
+    def front_end(self, name: str, repetitions: Optional[int] = None, timeout: Optional[int] = None) -> None:
+        pvl_name = self.version_to_file_name(name, 'front')
+        self.run_verification(pvl_name, repetitions, timeout)
+
+    def back_end(self, name: str, mem: bool = False, repetitions: Optional[int] = None, timeout: Optional[int] = None, version: str ='all') -> None:
+        if mem:
+            testing = self.mem_versions[name] if version == 'all' else [version]
+        else:
             testing = self.versions[name] if version == 'all' else [version]
-            for v in testing:
-                pvl_name = name + '_pvl-' + v + '.pvl'
-                self.verification_times[name][v] = []
-                for i in range(0,n):
-                    verificationTime = runCommand('vct --backend-option --conditionalizePermissions --silicon-quiet build/' + pvl_name, timeout=t)
-                    self.verification_times[name][v].append(verificationTime)
-                    print(verificationTime)
-        except Exception as e:
-            print(e)
+        for v in testing:
+            pvl_name = self.version_to_file_name(name, v, mem)
+            self.run_verification(pvl_name, repetitions, timeout)
 
-    def save_results(self, prefix: str, name: str) -> None:
-        pre = f'{prefix}_{name}'
+    def save_results(self, prefix: str) -> None:
+        pre = f'{prefix}'
         with open(pre + '_results_verification.json', 'w') as f:
-            f.write(json.dumps(self.verification_times[name]))
-        with open(pre + '_compilation.json', 'w') as f:
-            f.write(json.dumps(self.compilation_times[name]))
+            f.write(json.dumps(self.verification_times))
         with open(pre + '_line_info.json', 'w') as f:
-            f.write(json.dumps(self.line_infos[name], cls=Encoder))
+            f.write(json.dumps(self.line_infos, cls=Encoder))
 
-    def save_all_results(self, prefix: str) -> None:
-        for n in self.versions:
-            self.save_result(prefix, n)
-
-    def load_results(self, prefix, name) -> None:
-        results_verification, compilation, line_info = read_results(prefix, name)
-        self.verification_times[name] = results_verification
-        self.compilation_times[name] = compilation
-        self.line_infos[name] = line_info
-
-    def load_all_results(self, prefix: str) -> None:
-        for n in self.versions:
-            self.load_results(prefix, n)
+    def load_results(self, prefix: str) -> None:
+        results_verification, line_info = read_results(prefix)
+        self.verification_times = results_verification
+        self.line_infos = line_info
 
     def make_table(self, directivesUsed: Dict[str, Dict[str,str]], halideLoC: Dict[str, int], 
             halideAnn: Dict[str, int], scheduleLoC: Dict[str, Dict[str,int]]
@@ -256,14 +241,16 @@ class Experiments:
         for name in self.versions:
             for v in self.versions[name]:
                 row = ""
+                filename = self.version_to_file_name(name, v)
+
                 if v == self.versions[name][0]:
                     shortname = name
                     if name == 'conv_layer':
                         shortname = 'conv\_'
                     if name == 'auto_viz':
                         shortname = 'auto\_'
-                    t, passes, failt, fils, timeouts = get_average(self.verification_times[name]['front'])
-                    row += f"{shortname} & V{v} & {halideLoC[name]} & {halideAnn[name]} & {round(t)} & 0 & "
+                    t = average_to_str(self.verification_times[filename], filename)
+                    row += f"{shortname} & V{v} & {halideLoC[name]} & {halideAnn[name]} & {t} & 0 & "
                 else:
                     if name == 'conv_layer' and v == self.versions[name][1]:
                         row += "layer"
@@ -274,23 +261,14 @@ class Experiments:
                     
                 full_name = name + '-' + v
                 
-                row += f"{self.line_infos[name][v].lines_of_code} &"
-                row += f"{self.line_infos[name][v].nr_annotations} &" 
-                row += f"{self.line_infos[name][v].loops} & "
+                row += f"{self.line_infos[filename].lines_of_code} &"
+                row += f"{self.line_infos[filename].nr_annotations} &" 
+                row += f"{self.line_infos[filename].loops} & "
                 
-                res = self.verification_times[name][v]
-                t, passes, failtime, fails, timeouts = get_average(res)
-                if((passes > 0 and passes < len(res))):
-                    print(f"inconsistent results for {name} version V{v}")
-                    print(f"avr_total, passes, failtime, fails, timeouts: {t, passes, failtime, fails, timeouts}")
-                    row += " F & "
-                if(passes == len(res) and t != None):
-                    row += f"{round(t)} & "
-                elif(fails > 0):
-                    row += " F & "
-                elif(timeouts > 0):
-                    row += "T.O. & "
-                row += str(round(self.line_infos[name][v].nr_annotations / halideAnn[name], 1)) + "x" + r"\\ \hline"
+                res = self.verification_times[filename]
+                t = average_to_str(self.verification_times[filename], filename)
+                row += f"{t} & "
+                row += str(round(self.line_infos[filename].nr_annotations / halideAnn[name], 1)) + "x" + r"\\ \hline"
                 rows.append(row)
             rows.append ("\hline")
         rows.append(r"\end{tabular}")
@@ -306,9 +284,9 @@ class Experiments:
 
 # From https://stackoverflow.com/questions/19470099/view-pdf-image-in-an-ipython-notebook
 class PDF(object):
-  def __init__(self, pdf, size=(200,200)):
+  def __init__(self, pdf, size=(200,200)): # type: ignore
     self.pdf = pdf
     self.size = size
 
-  def _repr_html_(self):
+  def _repr_html_(self)->str:
     return '<iframe src={0} width={1[0]} height={1[1]}></iframe>'.format(self.pdf, self.size)
